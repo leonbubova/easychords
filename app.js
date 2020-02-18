9fa436828d85d8c15ffc928063b45f00a8504106
Leon Bubova <bubova.leon@gmx.de>
2020-02-18 22:52:40 +0100

    implented first checking and functioning version

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
 
 /**
  * chords is an array of arrays filled with chords (who wouldve guessed).
- * chords[0] is filled with all the chords you can play easily open on guitar
+ * chords[0] is filled with all the chords you can easily play open on guitar
  * for chords[0] this would mean [0, 2, 14, 4, 16, 7, 9, 21] or translated
  * into real chords:
  * [C, D, Dm, E, Em, G, A, Am]
@@ -31,25 +31,33 @@
  */
 var chords = [
     [0, 2, 14, 4, 16, 7, 9, 21],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
+    [1, 3, 15, 5, 17, 8, 10, 22],
+    [2, 4, 16, 6, 18, 9, 11, 23],
+    [3, 5, 17, 7, 19, 10, 0, 12],
+    [4, 6, 18, 8, 20, 11, 1, 13],
+    [5, 7, 19, 9, 21, 0, 2, 14],
+    [6, 8, 20, 10, 22, 1, 3, 15],
+    [7, 9, 21, 11, 23, 2, 4, 16],
+    [8, 10, 22, 0, 12, 3, 5, 17],
+    [9, 11, 23, 1, 13, 4, 6, 18],
+    [10, 0, 12, 2, 14, 5, 7, 19],
+    [11, 1, 13, 3, 15, 6, 8, 20]
 ];
+//Cism E B Fm
+chordsequence = [14, 5, 0, 18];
+easychords = [];
 
-//filling other slots of array via code cause i'm lazy
-for (i = 1; i < 12; i++){
-    for (j = 0; j < 8; i++){
-        chords[i][j] = chords[i][j] + 1; 
+for(i = 0; i < 11; i++){
+    if(
+        chords[i].includes(chordsequence[0]) &&
+        chords[i].includes(chordsequence[1]) &&
+        chords[i].includes(chordsequence[2]) 
+    ) {
+        console.log(easychords.push(i));
     }
 }
+
+console.log(easychords);
 
 function createButton(text){
     var btn = document.createElement('button');
